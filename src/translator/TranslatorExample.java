c7701208d52c29c902e93c7e03e1748375888b2d
unknown <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2019-05-24 16:11:56 +0530

    first commit

--- src/translator/TranslatorExample.java
+++ src/translator/TranslatorExample.java
@@ -7,23 +7,15 @@ import com.darkprograms.speech.translator.GoogleTranslate;
 public class TranslatorExample {
 	
 	public static void main(String[] args) {
-		
-		//Read this ma bro :)
-		//When you go on google translate website and you translate from English to Igbo for example
-		//you can see the url is :
-		// https://translate.google.com/#en/ig/How%20are%20you
-		//so the code for IGBO is "ig"
-		//see my examples below , i will make tutorial on youtube don't worry
-		
 		try {
 			//English to IGBO
-			System.out.println(GoogleTranslate.translate("ig", "Time flies always"));
+			System.out.println(GoogleTranslate.translate("ig", "Time flies always,Happy"));
 			
 			//English to HAUSA
-			System.out.println(GoogleTranslate.translate("ha", "Time flies always"));
+			System.out.println(GoogleTranslate.translate("ha", "Time flies always,Happy"));
 			
 			//English to Yoruba
-			System.out.println(GoogleTranslate.translate("yo", "Time flies always"));
+			System.out.println(GoogleTranslate.translate("yo", "Time flies always,Happy"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
